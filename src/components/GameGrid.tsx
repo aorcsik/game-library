c51bd88a3693c6efb501cc0596ccc6d5cb214374
Antal Orcsik <aorcsik@gmail.com>
2025-05-29 17:23:57 +0200

    update mobile layouts

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,6 @@
-import { Platform, PlatformList, PurchasedGame } from '../lib/PurchaseService';
-import GameRowTitle from './GameRowTitle';
-import Image from 'next/image';
-import { memo } from 'react';
+import { PlatformList, PurchasedGame } from '../lib/PurchaseService';
+import GameRow from './GameRow';
+import { memo, ReactNode } from 'react';
 
 
 interface GameGridProps {
@@ -9,64 +8,13 @@ interface GameGridProps {
   platforms: PlatformList;
 }
 
-const GameGrid: React.FC<GameGridProps> = ({ purchasedGames, platforms }) => {
+const GameGrid = ({ purchasedGames, platforms }: GameGridProps): ReactNode => {
   const sortedGames = [...purchasedGames].sort((a, b) => {
     return a.key.localeCompare(b.key, undefined, { numeric: true });
   });
 
   return sortedGames.map(game => {
-    return (
-      <div 
-        id={game.key} 
-        className="game-row" 
-        key={game.key}
-      >
-        <div className="game-row-title">
-          <GameRowTitle game={game} />
-        </div>
-        
-        {Object.keys(platforms).map(k => {
-          const platform = k as Platform;
-          const platformPurchase = game.purchases.find(p => p.platform === platform);
-          
-          if (platformPurchase) {
-            const { title, cover, collection, physical, logo: platformLogo } = platformPurchase;
-            
-            return (
-              <div className={`game-cell game-card game-${platform}`} key={platform}>
-                <div className="game-cover">
-                  <Image 
-                    className="game-cover-image"
-                    src={cover} 
-                    alt={`${title} cover`}
-                    loading="lazy"
-                    width={120}
-                    height={160}
-                  />
-                </div>
-                <div className="game-info">
-                  <div className="game-title">
-                    {title}
-                    {collection && (
-                      <div className="game-collection">({collection})</div>
-                    )}
-                  </div>
-                  <i className={`game-platform ${platformLogo}-logo ${physical ? 'physical' : ''}`} />
-                </div>
-              </div>
-            );
-          } else {
-            return (
-              <div className={`game-cell game-placeholder game-${platform}`} key={platform}>
-                <div className="game-info">
-                  <i className={`game-platform ${platform}-logo`} />
-                </div>
-              </div>
-            );
-          }
-        })}
-      </div>
-    );
+    return <GameRow game={game} key={game.key} platforms={platforms} />;
   });
 };
 
---
+++ src/components/GameRow.tsx
@@ -0,0 +1,71 @@
+import { ReactNode, useState } from 'react';
+import { Platform, PlatformList, PurchasedGame } from '../lib/PurchaseService';
+import GameRowTitle from './GameRowTitle';
+import Image from 'next/image';
+
+type GameRowProps = {
+  game: PurchasedGame;
+  platforms: PlatformList;
+};
+
+const GameRow = ({ game, platforms }: GameRowProps): ReactNode => {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = (): void => setOpen(!open);
+
+  return (
+    <div 
+      id={game.key} 
+      className={`game-row ${open ? 'game-row-open' : ''}`} 
+      key={game.key}
+      onClick={toggleOpen}
+    >
+      <div className="game-row-title">
+        <GameRowTitle game={game} />
+      </div>
+      
+      {Object.keys(platforms).map(k => {
+        const platform = k as Platform;
+        const platformPurchase = game.purchases.find(p => p.platform === platform);
+        
+        if (platformPurchase) {
+          const { title, cover, collection, physical, logo: platformLogo } = platformPurchase;
+          
+          return (
+            <div className={`game-cell game-card game-${platform}`} key={platform}>
+              <div className="game-cover">
+                <Image 
+                  className="game-cover-image"
+                  src={cover} 
+                  alt={`${title} cover`}
+                  loading="lazy"
+                  width={120}
+                  height={160}
+                />
+              </div>
+              <div className="game-info">
+                <div className="game-title">
+                  {title}
+                  {collection && (
+                    <div className="game-collection">({collection})</div>
+                  )}
+                </div>
+                <i className={`game-platform ${platformLogo}-logo ${physical ? 'physical' : ''}`} />
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <div className={`game-cell game-placeholder game-${platform}`} key={platform}>
+              <div className="game-info">
+                <i className={`game-platform ${platform}-logo`} />
+              </div>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default GameRow;
